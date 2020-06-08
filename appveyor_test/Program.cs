b8b7e72f6614215b0caac16c9a59929c14be31be
dasraf9 <dasraf9@users.noreply.github.com>
2020-06-08 17:53:15 +0300

    Update Program.cs

--- appveyor_test/Program.cs
+++ appveyor_test/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace appveyor_test
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World");
-			Console.WriteLine("Olleh");
+			Console.WriteLine("Dlrow Olleh");
 			Console.ReadKey();
 		}
 	}
